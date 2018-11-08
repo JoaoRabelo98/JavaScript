63368d2ec50b1ede8a3cbf3bd6cfc7755a0d6c5c
JoãoRabelo98 <43295586+JoaoRabelo98@users.noreply.github.com>
2018-11-07 22:57:26 -0200

    ///Codigo documentado///Construtor iniciado///Alterado idioma das variaveis///

--- ProjetoCalc/scripts/controller/CalcController.js
+++ ProjetoCalc/scripts/controller/CalcController.js
@@ -1,21 +1,28 @@
 class CalcController{
 
     constructor(){
-        let _catchDate = new Date();
-        this._displayCalc = "0";
-        this._currentDate = _catchDate.toLocaleDateString("pt-BR"); 
+        let _catchDate = new Date(); //estamos instanciando a classe Date();
+        this._displayCalc = "0"; // atribuindo um valor padrao ao display
+        this._currentDate = _catchDate.toLocaleDateString("pt-BR"); //estamos indicando que a variavel local recebe a data atual.
+        this.initialize(); // toda vez que chamarmos o metodo, essa ação é executada. 
     }
 
-    get displayCalc(){
+    initialize(){ // start initialize
+
+
+
+    } // end initialize
+
+    get displayCalc(){ // start getDisplay
         return this._displayCalc;
-    }
+    }//end getDisplay
 
-    set displayCalc(displayCalc){
+    set displayCalc(displayCalc){ //start setDisplay
         this._displayCalc = displayCalc;
-    }
+    } // end setDisplay
 
-    get dataAtual(){
+    get currentDate(){ // start getCurrentDate
         return this._currentDate;
-    }
+    } // end getCurrentDate
 
 }
