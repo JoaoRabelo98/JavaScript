ffd8407eaf3382e77abd39ecac3f61d6047415bc
Joao Rabelo <joaorabello99@gmail.com>
2018-11-10 23:36:06 -0200

    ok

---
+++ ProjetoUsers/index.js
@@ -0,0 +1,17 @@
+let inputName = document.querySelector("#exampleInputName"); 
+let inputCheckedGen = document.querySelectorAll("#form-user-create [name=gender]:checked");
+let inputDateNasc = document.querySelector("#exampleInputBirth");
+let inputCuntry  = document.querySelector("#exampleInputCountry");
+let inputEmail = document.querySelector("#exampleInputEmail");
+let inputPasswd = document.querySelector("#exampleInputPassword");
+let inputFile = document.querySelector("#exampleinputFile");
+let inputAdmin = document.querySelector("#exampleInputAdmin");
+
+inputName.value = "João Vitor Rabelo Rezende";
+inputCheckedGen.checked = true; 
+inputDateNasc.value = "1998-03-05"
+inputCuntry.value = "China";
+inputEmail.value = "joaorabello99@gmail.com";
+inputPasswd.value = "joao1234";
+inputAdmin.checked = true; 
+
